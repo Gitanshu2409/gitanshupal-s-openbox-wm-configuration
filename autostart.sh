1fe45907298e547004fef37bd2d1ed5b7df5aec7
Gitanshu2409 <86179904+Gitanshu2409@users.noreply.github.com>
2022-06-02 10:07:40 +0530

    Add files via upload

---
+++ autostart.sh
@@ -0,0 +1,15 @@
+# gitanshupal's Openbox Autostart Script (autostart.sh) -----------
+
+# Bring up my panel.
+lxpanel&
+
+# Launch network management applet.
+nm-applet&
+
+# Bring the wallpaper back.
+nitrogen --restore&
+
+# Bring my application dock back.
+plank -n dock2&
+
+# Desktop Initialised ----------------------------------------------
